a3439d8219330462b43f96844112369b99e8da5b
Ostap Brehin <osbre@protonmail.com>
2026-01-31 22:49:22 +0000

    Vite chunk size to avoid serving many files

--- vite.config.js
+++ vite.config.js
@@ -18,4 +18,11 @@ export default defineConfig({
             },
         }),
     ],
+    build: {
+        rollupOptions: {
+            output: {
+                experimentalMinChunkSize: 100_000, // 10 KB
+            },
+        },
+    },
 });
